059936560041b5c5d02dd87680f93877f5bc46fa
cyan.coral-dell-win8 <coral.script@foxmail.com>
2015-07-08 11:48:14 +0800

    qiubai

--- qiubai.py
+++ qiubai.py
@@ -9,25 +9,28 @@ import sys
 
 class Spider:
 
-    def __init__(self):
-        self.page = 1
-
-
     def GetPage(self,page):
         url ='http://m.qiushibaike.com/textnew/page/'+str(page)
         user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         headers = {'User-Agent':user_agent}
         req = urllib2.Request(url,headers = headers)
-        response = urllib2.urlopen(req)
+        # print url
+        try:
+          response = urllib2.urlopen(req)
+        except Exception, e:
+          return False
         # page = response.read().decode('utf-8')
         pageContent = response.read()
         items = re.findall('<div class="content">\n\n(.*?)<!--(.*?)-->\n\n</div>',pageContent,re.S)
+        if items.__len__() == 0:
+            return False
         itemCollection = []
         for item in items:
             itemCollection.append([item[1],item[0].replace("<br/>","\n")])
-            print u"时间:",item[1],"\n", item[0].replace("<br/>","\n")
+            # print u"时间:",item[1],"\n", item[0].replace("<br/>","\n")
         SaveText(page,itemCollection)
-        return itemCollection
+        page+=1
+        return True
 
 def SaveText(page,msg):
     # 1页存一个txt
@@ -39,6 +42,8 @@ def SaveText(page,msg):
     file.writelines(['日期:%s\n%s\n' %(x[0],x[1]) for x in msg])
     file.close()
 
+print "start"
 Model = Spider()
-for i in range(1,35):
-    Model.GetPage(i)
+i = 1
+while(Model.GetPage(i)):
+    i+=1
